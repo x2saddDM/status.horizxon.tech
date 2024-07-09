e47b1b8cf09fa0dbb2abbd03d0b5b01070a8e0ea
x2saddDM <166689399+x2saddDM@users.noreply.github.com>
2024-07-09 23:01:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,39 +1,12 @@
 module.exports = {
   nodes: [
      {
-      host: "lava.horizxon.tech",
-      password: "horizxon.tech",
-      port: 80,
+      host: "45.140.165.216",
+      password: "youshallnotpass",
+      port: 1046,
       retryDelay: 1,
-      identifier: "lava.horizxon.tech・(NO-SSL)",
+      identifier: "Yuuki Nodes [v3]",
       secure: false,
-    },
-
-     {
-      host: "lava1.horizxon.tech",
-      password: "horizxon.tech",
-      port: 443,
-      retryDelay: 1,
-      identifier: "lava1.horizxon.tech・(SSL)",
-      secure: true,
-    },
-
-    {
-      host: "lava2.horizxon.tech",
-      password: "horizxon.tech",
-      port: 443,
-      retryDelay: 1,
-      identifier: "lava2.horizxon.tech・(SSL)",
-      secure: true,
-    },
-
-    {
-      host: "lava3.horizxon.tech",
-      password: "horizxon.tech",
-      port: 443,
-      retryDelay: 1,
-      identifier: "lava3.horizxon.tech・(SSL)",
-      secure: true,
-    },   
+    } 
   ],
 };
